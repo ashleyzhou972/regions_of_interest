e8ee5d43c7f088a4a9e5f9d8cdb51a4521acc7bb
Ashley Zhou <naihui.zhou@tempus.com>
2021-12-07 17:41:09 -0600

    add R setup files

---
+++ install.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
+install.packages('data.table')
+install.packages('dplyr')
+install.packages('GenomicRanges')
+
